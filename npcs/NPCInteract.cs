6954a1a16882043e82c6bb037427288f550c86b4
kyle lugo <kjl56@zips.uakron.edu>
2021-03-13 21:31:22 -0500

    some npc stuff
    add some npc stuff for interactions

---
+++ npcs/NPCInteract.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteract : MonoBehaviour
+{
+  //for displaying talkPrompt
+  public Animator animator;
+  //for passing in simple dialogue lines
+  public DialogueTrigger dialogueTrigger;
+
+  void OnTriggerEnter(Collider collider) {
+    if (collider.gameObject.tag == "Player") {
+      //display button prompt
+      animator.SetBool("nearbyNPC", true);
+    }
+  }
+
+  void OnTriggerStay(Collider collider) {
+    //if player is in range of npc
+    if (collider.gameObject.tag == "Player") {
+      if (Input.GetKeyDown(KeyCode.E)) {
+        dialogueTrigger.TriggerDialogue();
+      }
+    }
+  }
+
+  void OnTriggerExit(Collider collider) {
+    if (collider.gameObject.tag == "Player") {
+      //remove button prompt
+      animator.SetBool("nearbyNPC", false);
+    }
+  }
+}
