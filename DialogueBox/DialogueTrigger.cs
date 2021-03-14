cb8b5a73edd3bfcc201a5e8280e5b3499f1dcf69
kyle lugo <kjl56@zips.uakron.edu>
2021-03-13 21:28:19 -0500

    dialog manager
    add a general purpose dialog manager

---
+++ DialogueBox/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//requires u to make an empty game object in the scene
+//name it "DialogueManager"
+//and attatch the DialogueManager.cs script to it
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue() {
+      FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
